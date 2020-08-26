279e6ef377cf7a87cb67a89fd555c7c25c5b3b38
roveroc <roveroc@163.com>
2020-08-26 17:20:04 +0800

    新建供货大厅下部tableview的cell

---
+++ PeachBlossom_iOS/PeachBlossom_iOS/View/Home/Supply/SupplyGoodsCell.m
@@ -0,0 +1,24 @@
+//
+//  SupplyGoodsCell.m
+//  PeachBlossom_iOS
+//
+//  Created by rover on 2020/8/26.
+//  Copyright © 2020 zengyongbing. All rights reserved.
+//
+
+#import "SupplyGoodsCell.h"
+
+@implementation SupplyGoodsCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
