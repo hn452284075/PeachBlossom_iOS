8234fcd6e25b4738134f715fd118b46e0878a67f
roveroc <roveroc@163.com>
2020-08-26 16:50:14 +0800

    完善供应大厅界面

--- PeachBlossom_iOS/PeachBlossom_iOS/ViewController/Home/Supply/SupplyViewController.m
+++ PeachBlossom_iOS/PeachBlossom_iOS/ViewController/Home/Supply/SupplyViewController.m
@@ -12,12 +12,22 @@
 
 @interface SupplyViewController ()
 
+//农产品  农资种苗   农机具  三个类别
+@property (nonatomic, strong) UIButton *class_btn1;
+@property (nonatomic, strong) UIButton *class_btn2;
+@property (nonatomic, strong) UIButton *class_btn3;
+
+//选中某一个类别后下方的小图标
+@property (nonatomic, strong) UIView   *selectedView;
+
 @end
 
 @implementation SupplyViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = kGetColor(0xf7, 0xf7, 0xf7);
     
     //顶部背景图片
     UIImageView *topImg = [[UIImageView alloc] init];
@@ -93,6 +103,132 @@
     [adBtn setBackgroundImage:IMAGE(@"supply_banner") forState:UIControlStateNormal];
     
     //中间部分 产品种类view
+    UIView *productView = [[UIView alloc] init];
+    [self.view addSubview:productView];
+    [productView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).offset(251.5);
+        make.left.equalTo(self.view.mas_left).offset(14);
+        make.right.equalTo(self.view.mas_right).offset(-14);
+        make.height.mas_equalTo(228);
+    }];
+    productView.backgroundColor = [UIColor whiteColor];
+    
+    self.class_btn1 = [self getCustomBtn:self.class_btn1 title:@"农产品" tag:1];
+    self.class_btn2 = [self getCustomBtn:self.class_btn2 title:@"农资种苗" tag:2];
+    self.class_btn3 = [self getCustomBtn:self.class_btn3 title:@"农机具" tag:3];
+    
+    [productView addSubview:self.class_btn1];
+    [self.class_btn1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(productView.mas_top).offset(13.5);
+        make.left.equalTo(productView.mas_left);
+        make.width.mas_equalTo(productView.mas_width).dividedBy(3);
+        make.height.mas_equalTo(16);
+    }];
+    
+    [productView addSubview:self.class_btn2];
+    [self.class_btn2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view.mas_centerX);
+        make.centerY.equalTo(self.class_btn1);
+        make.width.mas_equalTo(productView.mas_width).dividedBy(3);
+        make.height.mas_equalTo(16);
+    }];
+    
+    [productView addSubview:self.class_btn3];
+    [self.class_btn3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.class_btn1);
+        make.right.equalTo(productView.mas_right);
+        make.width.mas_equalTo(productView.mas_width).dividedBy(3);
+        make.height.mas_equalTo(16);
+    }];
+    
+    self.selectedView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 29, 2)];
+    self.selectedView.backgroundColor = kGetColor(0x47, 0xc6, 0x7c);
+    [productView addSubview:self.selectedView];
+    
+    self.class_btn1.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
+    self.class_btn2.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+    self.class_btn3.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.class_btn1 sendActionsForControlEvents:UIControlEventTouchUpInside];
+    });
+    
+    
+    //中间产品分类
+    NSMutableArray *classImg_arr1 = [[NSMutableArray alloc] init];
+    for(int i=0;i<8;i++)
+    {
+        [classImg_arr1 addObject:@"home-icon_1"];
+    }
+    NSMutableArray *className_arr1 = [[NSMutableArray alloc] init];
+    for(int i=0;i<8;i++)
+    {
+        [className_arr1 addObject:@"苹果"];
+    }
+    NSMutableArray *classView_arr1 = [[NSMutableArray alloc] init];
+    for(int i=0;i<8;i++)
+    {
+        UIView *view = [self getCustomClassView:[UIImage imageNamed:[classImg_arr1 objectAtIndex:i]] tag:i+100 title:[className_arr1 objectAtIndex:i]];
+        [classView_arr1 addObject:view];
+        [productView addSubview:view];
+    }
+    //水平方向宽度固定等间隔
+    int row = (int)classView_arr1.count / 5;
+    if(classView_arr1.count % 5 != 0)
+        row +=1;
+    UIView *v = [classView_arr1 objectAtIndex:0];
+    int x = 0;
+    int y = 40;
+    int w = v.frame.size.width;
+    int h = v.frame.size.height;
+    for(int i=0;i<row;i++)
+    {
+        int temp = 0;
+        for(int j=5*i;j<classView_arr1.count;j++)
+        {
+            UIView *v = [classView_arr1 objectAtIndex:j];
+            v.frame = CGRectMake(x+w*temp++, y+h*i, w, h);
+        }
+    }
+    
+    //产地
+    UIView *placeView = [[UIView alloc] init];
+    [self.view addSubview:placeView];
+    [placeView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(productView.mas_bottom).offset(18.5);
+        make.left.equalTo(self.view.mas_left).offset(14);
+        make.right.equalTo(self.view.mas_right).offset(-14);
+        make.height.mas_equalTo(36);
+    }];
+    placeView.backgroundColor = [UIColor whiteColor];
+    
+    UILabel *placeLabel = [[UILabel alloc] init];
+    [placeView addSubview:placeLabel];
+    [placeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(placeView.mas_centerY);
+        make.left.equalTo(placeView.mas_left).offset(14);
+    }];
+    placeLabel.text = @"产地";
+    placeLabel.font = [UIFont boldSystemFontOfSize:16];
+    placeLabel.textColor = kGetColor(0x22, 0x22, 0x22);
+    
+    NSArray *placeArr = [[NSArray alloc] initWithObjects:@"附近",@"湖南",@"湖北",@"广西",@"山西",@"河北", nil];
+    NSMutableArray *placeItemArray = [[NSMutableArray alloc] init];
+    for(int i=0;i<placeArr.count;i++)
+    {
+        UIButton *btn = [[UIButton alloc] init];
+        [btn setTitle:[placeArr objectAtIndex:i] forState:UIControlStateNormal];
+        [btn setTitleColor:kGetColor(0x66, 0x66, 0x66) forState:UIControlStateNormal];
+        btn.titleLabel.font = [UIFont systemFontOfSize:14];
+        [placeView addSubview:btn];
+        [placeItemArray addObject:btn];
+        btn.tag = i;
+        [btn addTarget:self action:@selector(placeBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    [placeItemArray mas_distributeViewsAlongAxis:MASAxisTypeHorizontal withFixedItemLength:(kScreenWidth-60-28)/6 leadSpacing:60 tailSpacing:2];
+    [placeItemArray mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(placeView.mas_centerY);
+    }];
     
 }
 
@@ -110,10 +246,85 @@
     
 }
 
+#pragma mark ------------------------ 中间部分农产品、农资种苗、农机具点击事件
+- (void)classBtnClicked:(id)sender
+{
+    [self.class_btn1 setTitleColor:kGetColor(0x22, 0x22, 0x22) forState:UIControlStateNormal];
+    [self.class_btn2 setTitleColor:kGetColor(0x22, 0x22, 0x22) forState:UIControlStateNormal];
+    [self.class_btn3 setTitleColor:kGetColor(0x22, 0x22, 0x22) forState:UIControlStateNormal];
+    
+    UIButton *btn = (UIButton *)sender;
+    [btn setTitleColor:kGetColor(0x47, 0xc6, 0x7c) forState:UIControlStateNormal];
+    
+    
+    switch (btn.tag) {
+        case 1:
+        {
+            self.selectedView.center = CGPointMake(btn.frame.origin.x+btn.frame.size.width-25, btn.center.y+15);
+        }
+            break;
+        case 2:
+        {
+            self.selectedView.center = CGPointMake(btn.center.x, btn.center.y+15);
+        }
+            break;
+        case 3:
+        {
+            self.selectedView.center = CGPointMake(btn.frame.origin.x+25, btn.center.y+15);
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark ------------------------ 切换产地按钮点击事件
+- (void)placeBtnClicked:(id)sender
+{
+    
+
+}
+
+
+#pragma mark ------------------------ 种类分类按钮点击事件
+- (void)productClassViewClicked:(id)sender
+{
+    
+}
 
 #pragma mark ------------------------Init---------------------------------
  
 #pragma mark ------------------------Private------------------------------
+- (UIButton *)getCustomBtn:(UIButton *)btn title:(NSString *)string tag:(int)tag
+{
+    btn = [[UIButton alloc] init];
+    btn.tag = tag;
+    [btn setTitle:string forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(classBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [btn setTitleColor:kGetColor(0x22, 0x22, 0x22) forState:UIControlStateNormal];
+    btn.titleLabel.font = [UIFont systemFontOfSize:16];
+    return btn;
+}
+
+- (UIView *)getCustomClassView:(UIImage *)img tag:(int)tag title:(NSString *)string
+{
+    int w = (kScreenWidth-28)/5;
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, w, w+20)];
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, w, w)];
+    [btn setImage:img forState:UIControlStateNormal];
+    btn.tag = tag;
+    [view addSubview:btn];
+    [btn addTarget:self action:@selector(productClassViewClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(0, w-2, w, 14)];
+    lab.text = string;
+    lab.font = [UIFont systemFontOfSize:14];
+    lab.textAlignment = NSTextAlignmentCenter;
+    [view addSubview:lab];
+    
+    return view;
+}
  
 #pragma mark ------------------------Api----------------------------------
 
