9cfb4ac9fb1691b6029c826a46c6372a9b6ecd6f
曾勇兵 <>
2020-08-31 18:13:19 +0800

    解决冲突

---
+++ PeachBlossom_iOS/PeachBlossom_iOS/View/Store/Shelves/ShelvesCell.h
@@ -0,0 +1,17 @@
+//
+//  ShelvesCell.h
+//  PeachBlossom_iOS
+//
+//  Created by 曾勇兵 on 2020/8/31.
+//  Copyright © 2020 zengyongbing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShelvesCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
